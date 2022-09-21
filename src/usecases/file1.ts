5adb73a88c902da223604e363ea1552a7e48a2b0
1z0ldyck <lucase2040@gmail.com>
2022-09-21 16:19:06 -0300

    Altera file1.ts

--- src/usecases/file1.ts
+++ src/usecases/file1.ts
@@ -5,13 +5,15 @@ import loadModules from '../lib/loadModules';
 const app = express();
 
 
+// Essa é uma implementação feita pelo usuário, para declarar que há novos métodos na instância da aplicação.
+
 declare module 'express-serve-static-core' {
   export interface Express {
     print: any
   }
 }
 
-(async function run(module) {
+(async function run() {
   await loadModules(app, {
     print: '../usecases/file2.ts'
   })
